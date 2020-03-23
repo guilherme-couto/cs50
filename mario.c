46ed06801bf22ac0c842ecb2105dd5d9251174f3
guilherme-couto <guilherme-couto@users.noreply.github.com>
2020-03-23 16:32:02 +0000

    automated commit by submit50 [lang=C.UTF-8]

---
+++ mario.c
@@ -0,0 +1,51 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("How many bricks tall does the mario's pyramid will have?\n");
+    int height = get_int("(insert a number between 1 and 8): ");
+    
+    if (height < 1 || height > 8)
+    {
+        do
+        {
+            height = get_int("(insert a number between 1 and 8): ");
+        }
+        while (height < 1 || height > 8);
+    }
+
+    for (int i = 1; i <= height; i++) //lines
+    {
+        //first pyramid
+        for (int j = 1; j <= height; j++)
+        {
+            if (j + i > height) //condition to brick
+            {
+                printf("#"); //brick
+            }
+            else
+            {
+                printf(" "); //empty
+            }
+        }
+        
+        //gap
+        printf("  ");
+
+        //second pyramid
+        for (int j = height + 3; j <= (2 * height) + 2; j++)
+        {
+            if (j <= (i + height + 2))
+            {
+                printf("#"); //brick
+            }
+            else
+            {
+                printf(" "); //empty
+            }
+        }
+
+        printf("\n"); //new line
+    }
+}
