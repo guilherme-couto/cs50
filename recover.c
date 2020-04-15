36e2350962d44d0b5c3c14280dc23e4f572a1615
guilherme-couto <guilherme-couto@users.noreply.github.com>
2020-04-15 23:49:28 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -39,7 +39,9 @@ int main(int argc, char *argv[])
     int i = 0;
     sprintf(argv[1], "%03i.jpg", i);
     FILE *img = fopen(argv[1], "w");
+    fwrite(arr, sizeof(BYTE), 512, img);
 
+    int c = 512;
     //continunar busca
     while ('TRUE')
     {
@@ -47,32 +49,36 @@ int main(int argc, char *argv[])
         if (arr[0] == 0xff && arr[1] == 0xd8 && arr[2] == 0xff && (arr[3] & 0xf0) == 0xe0)
         {
 
-
-            fwrite(arr, sizeof(BYTE), 512, img);
-            fread(arr, sizeof(BYTE), 512, file);
-
+            //fwrite(arr, sizeof(BYTE), 512, img);
+            //c = fread(arr, sizeof(BYTE), c, file);
+            fwrite(arr, sizeof(BYTE), fread(arr, sizeof(BYTE), 512, file), img);
 
             while (arr[0] != 0xff || arr[1] != 0xd8 || arr[2] != 0xff || (arr[3] & 0xf0) != 0xe0)
             {
-                fwrite(arr, sizeof(BYTE), 512, img);
-                fread(arr, sizeof(BYTE), 512, file);
-                //printf("2- %i %i %i %i\n", arr[0], arr[1], arr[2], arr[3]);
+                c = fread(arr, sizeof(BYTE), c, file);
+
+                if (c < 512)
+                {
+                    fwrite(arr, sizeof(BYTE), c, img);
+                    fclose(img);
+                    fclose(file);
+                    return 0;
+                }
+                else
+                {
+                    fwrite(arr, sizeof(BYTE), c, img);
+                    //fread(arr, sizeof(BYTE), 512, file);
+                }
             }
 
-            //printf("Alou\n");
             fclose(img);
             i ++;
-
-            if (i == 49)
-            {
-                fclose(file);
-                return 0;
-            }
 
             sprintf(argv[1], "%03i.jpg", i);
             img = fopen(argv[1], "w");
 
         }
+
 
     }
     //free(arr);
